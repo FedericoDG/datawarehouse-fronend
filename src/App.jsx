c46e03b181ddcfe32197de98f0f33c0dba6bce03
Federico Gonzalez <alambratore@hotmail.com>
2022-08-02 01:50:28 -0300

    MUI Theme configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { Provider } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import lightTheme from './themes/light-theme';
 
 import AppRoutes from './routes/AppRoutes';
 import store from './app/store';
+import Footer from './components/ui/Footer';
 
 const App = () => (
   <Provider store={store}>
-    <AppRoutes />
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <AppRoutes />
+      <Footer />
+    </ThemeProvider>
   </Provider>
 );
 
