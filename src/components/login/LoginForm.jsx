eb825197fbca79fea04a331865c97485b3d75be2
Federico Gonzalez <alambratore@hotmail.com>
2022-08-03 04:15:25 -0300

    Login ready

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,7 +1,32 @@
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+
+import { authLogin } from '../../app/authSlice';
+import { isEmail } from '../../utils/validations';
+
 import LogoCompleto from '../../assets/logo-completo.png';
 
 const LoginForm = () => {
+  const [showErrors, setShowErrors] = useState(false);
+
+  const { auth } = useSelector((state) => state);
+
+  const { loading } = auth;
+
+  const dispatch = useDispatch();
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    register
+  } = useForm();
+
+  const onLogin = async (data) => {
+    await dispatch(authLogin(data));
+  };
+
   return (
     <>
       <Box
@@ -11,13 +36,31 @@ const LoginForm = () => {
         sx={{ transition: 'transform .5s', ':hover': { transform: 'scale(1.05)' } }}
         width='75%'
       />
-      <form style={{ width: '80%' }}>
+      <form style={{ width: '80%' }} onSubmit={handleSubmit(onLogin)}>
         <Stack p={1} spacing={1}>
           <Typography align='center' color='initial' fontWeight='light' variant='h6'>
             Que gusto tenerte aquí
           </Typography>
-          <TextField label='Email' name='mail' />
-          <TextField label='Contraseña' name='password' type='password' />
+          <TextField
+            autoFocus
+            label='Email'
+            {...register('email', {
+              required: 'Por favor ingrese su email',
+              validate: isEmail
+            })}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+          <TextField
+            label='Contraseña'
+            type='password'
+            {...register('password', {
+              required: 'Por favor ingrese su contraseña',
+              minLength: { value: 8, message: 'Mínimo 8 caracteres' }
+            })}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+          />
           <Button sx={{ paddingY: '12px' }} type='submit' variant='contained'>
             Ingresar
           </Button>
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,81 +1,30 @@
-import { useDispatch, useSelector } from 'react-redux';
 import Grid from '@mui/material/Grid';
 
 import LoginBanner from '../components/login/LoginBanner';
 import LoginForm from '../components/login/LoginForm';
-import { authLogin } from '../app/authSlice';
 
-const Login = () => {
-  const { auth } = useSelector((state) => state);
-
-  const { loading } = auth;
-
-  const dispatch = useDispatch();
-
-  const admin = {
-    email: 'admin@gmail.com',
-    password: 'Password1234!'
-  };
-
-  const user = {
-    email: 'user@gmail.com',
-    password: 'Password1234!'
-  };
-  return (
-    <Grid container>
-      <Grid
-        item
-        lg={3}
-        md={4}
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100vh',
-          justifyContent: 'center',
-          width: '100%'
-        }}
-        xs={12}
-      >
-        <LoginForm />
-        <button onClick={() => dispatch(authLogin(admin))} disabled={loading}>
-          Login Admin
-        </button>
-        <button onClick={() => dispatch(authLogin(user))} disabled={loading}>
-          Login User
-        </button>
-      </Grid>
-      <Grid item display={{ xs: 'none', md: 'block' }} lg={9} md={8}>
-        <LoginBanner />
-      </Grid>
+const Login = () => (
+  <Grid container>
+    <Grid
+      item
+      lg={3}
+      md={4}
+      sx={{
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        justifyContent: 'center',
+        width: '100%'
+      }}
+      xs={12}
+    >
+      <LoginForm />
     </Grid>
-  );
-};
+    <Grid item display={{ xs: 'none', md: 'block' }} lg={9} md={8}>
+      <LoginBanner />
+    </Grid>
+  </Grid>
+);
 
 export default Login;
-/* 
-  const { auth } = useSelector((state) => state);
-
-  const { loading } = auth;
-
-  const dispatch = useDispatch();
-
-  const admin = {
-    email: 'admin@gmail.com',
-    password: 'Password1234!'
-  };
-
-  const user = {
-    email: 'user@gmail.com',
-    password: 'Password1234!'
-  };
-
-
-
-       <button onClick={() => dispatch(authLogin(admin))} disabled={loading}>
-        Login Admin
-      </button>
-      <button onClick={() => dispatch(authLogin(user))} disabled={loading}>
-        Login User
-      </button>
-*/
