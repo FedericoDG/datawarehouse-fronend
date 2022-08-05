773f0fe9dd757f7b383a313360ce3b91ff0fed85
Federico Gonzalez <alambratore@hotmail.com>
2022-08-05 08:56:42 -0300

    Created a custom hook: useContacts

---
+++ src/hooks/useContacts.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { useAddContactMutation, useDeteleContactMutation, useEditContactMutation, useGetContactsQuery } from '../app/contactsApi';
+
+import defaultContact from '../constants/defaultContact';
+
+const useContects = () => {
+  const [activeData, setActiveData] = useState(defaultContact);
+  const [filter, setFilter] = useState('');
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState('');
+
+  const { data: contacts, isError, isLoading } = useGetContactsQuery(filter);
+  const [addContact] = useAddContactMutation();
+  const [editContact] = useEditContactMutation();
+  const [deleteContact] = useDeteleContactMutation();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setActiveData(defaultContact);
+  };
+
+  const handleAddContact = async (contact) => {
+    await addContact(contact);
+  };
+
+  const handleEditContact = async (contact) => {
+    await editContact(contact);
+  };
+
+  const handleDeleteContact = async (id) => {
+    await deleteContact(id);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFilter(search);
+  };
+
+  const handleReset = () => {
+    setSearch('');
+    setFilter('');
+  };
+
+  return {
+    activeData,
+    setActiveData,
+    open,
+    search,
+    setSearch,
+    contacts,
+    isError,
+    isLoading,
+    handleOpen,
+    handleClose,
+    handleAddContact,
+    handleEditContact,
+    handleDeleteContact,
+    handleSubmit,
+    handleReset
+  };
+};
+
+export default useContects;
--- src/views/Contacts.jsx
+++ src/views/Contacts.jsx
@@ -1,7 +1,4 @@
 import { Grid } from '@mui/material';
-import { useMemo, useState } from 'react';
-
-import { useAddContactMutation, useDeteleContactMutation, useEditContactMutation, useGetContactsQuery } from '../app/contactsApi';
 import Layout from '../components/ui/Layout';
 import ModalContacts from '../components/contacts/Modal';
 import Search from '../components/contacts/Seach';
@@ -9,86 +6,34 @@ import Table from '../components/contacts/Table';
 import Buttons from '../components/contacts/Buttons';
 import Spinner from '../components/ui/Spinner';
 
+import Error from '../components/ui/Error';
+import useContects from '../hooks/useContacts';
+
 const Contacts = () => {
-  const defaultContact = useMemo(
-    () => ({
-      id_contact: '',
-      name: '',
-      lastname: '',
-      email: '',
-      address: '',
-      phone: '',
-      preference_phone: 1,
-      preference_phone_name: 'sin preferencia',
-      linkedin: '',
-      preference_linkedin: 1,
-      preference_linkedin_name: 'sin preferencia',
-      facebook: '',
-      preference_facebook: 1,
-      preference_facebook_name: 'sin preferencia',
-      twitter: '',
-      preference_twitter: 1,
-      preference_twitter_name: 'sin preferencia',
-      instagram: '',
-      preference_instagram: 1,
-      preference_instagram_name: 'sin preferencia',
-      position: '',
-      interest: 50,
-      id_company: '',
-      company_name: '',
-      id_city: '',
-      city_name: '',
-      id_country: '',
-      country_name: '',
-      id_region: '',
-      region_name: ''
-    }),
-    []
-  );
-
-  const [activeData, setActiveData] = useState(defaultContact);
-  const [filter, setFilter] = useState('');
-  const [open, setOpen] = useState(false);
-  const [search, setSearch] = useState('');
-
-  const { data: contacts, isLoading } = useGetContactsQuery(filter);
-  const [addContact] = useAddContactMutation();
-  const [editContact] = useEditContactMutation();
-  const [deleteContact] = useDeteleContactMutation();
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-    setActiveData(defaultContact);
-  };
-
-  const handleAddContact = async (contact) => {
-    await addContact(contact);
-  };
-
-  const handleEditContact = async (contact) => {
-    await editContact(contact);
-  };
-
-  const handleDeleteContact = async (id) => {
-    await deleteContact(id);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFilter(search);
-  };
-
-  const handleReset = () => {
-    setSearch('');
-    setFilter('');
-  };
+  const {
+    activeData,
+    setActiveData,
+    open,
+    search,
+    setSearch,
+    contacts,
+    isError,
+    isLoading,
+    handleOpen,
+    handleClose,
+    handleAddContact,
+    handleEditContact,
+    handleDeleteContact,
+    handleSubmit,
+    handleReset
+  } = useContects();
 
   if (isLoading) {
-    return <Spinner height={786} />;
+    return <Spinner height={737} />;
+  }
+
+  if (isError) {
+    return <Error height={737} />;
   }
 
   return (
