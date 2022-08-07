a153477028a015e97cada680a7e4ccec8205564b
Federico Gonzalez <alambratore@hotmail.com>
2022-08-06 23:40:36 -0300

    Some files were renamed

---
+++ src/components/ui/ButtonGroup.jsx
@@ -0,0 +1,21 @@
+import { Button } from '@mui/material';
+import { CSVLink } from 'react-csv';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+
+const ButtonGroup = ({ contacts = [], handleOpen, title, csv = true }) => {
+  return (
+    <>
+      {csv && (
+        <Button variant='outlined' startIcon={<FileDownloadIcon />} sx={{ marginRight: 2 }}>
+          <CSVLink data={contacts} filename='my-file.csv' style={{ textDecoration: 'none' }}>
+            CSV
+          </CSVLink>
+        </Button>
+      )}
+      <Button size='large' variant='contained' sx={{ minWidth: 206, minHeight: 44 }} onClick={() => handleOpen()}>
+        {title}
+      </Button>
+    </>
+  );
+};
+export default ButtonGroup;
